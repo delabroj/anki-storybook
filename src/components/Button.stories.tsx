636f66c596b459bcd559c51683d0ed1d72d38458
delabroj <delabroj@users.noreply.github.com>
2020-04-14 14:58:08 -0600

    -

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,9 @@
+import React, { FC, ReactElement } from 'react';
+import Button from './Button';
+
+export default {
+    title: 'Button',
+    component: Button,
+};
+
+export const Default: FC = () => <Button onClick={() => alert('Clicked')} label="Click me" />;
