0b8a70aedaf90af96ace17eec44a20e08ae1b15c
delabroj <delabroj@users.noreply.github.com>
2020-04-14 15:08:12 -0600

    -

--- src/components/Button.stories.solution.tsx
+++ src/components/Button.stories.solution.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC } from 'react';
 import Button from './Button';
 import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 
@@ -8,6 +8,14 @@ export default {
     decorators: [withKnobs],
 };
 
+const displayGroup = 'Display';
+const behaviorGroup = 'Behavior';
+
 export const Default: FC = () => (
-    <Button onClick={() => alert('Clicked')} label={text('label', 'Click me')} disabled={boolean('disabled', false)} />
+    <Button
+        onClick={() => alert('Clicked')}
+        label={text('label', 'Click me', displayGroup)}
+        toolTip={text('toolTip', 'Hovering', displayGroup)}
+        disabled={boolean('disabled', false, behaviorGroup)}
+    />
 );
